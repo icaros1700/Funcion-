14d7ccc93f6d17709bffeb03596fc115558f750e
william Ruiz <william.a23@gmail.com>
2022-06-01 11:49:24 -0500

    primer commit de funcion suma

---
+++ suma/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int resultado;
+
+        resultado = suma (3,5,2);
+
+        System.out.println(resultado);
+        
+    }
+
+    public static int suma (int a, int b, int c) {
+
+        return a + b + c;
+
+    }
+}
+
